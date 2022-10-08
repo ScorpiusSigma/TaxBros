dca8b32170be3a6dc8970d6e30a2abd1a3bee9eb
Justin <justinleng_@hotmail.com>
2022-10-08 17:56:09 +0800

    [Edit] GlobalContext

--- pages/_app.js
+++ pages/_app.js
@@ -2,10 +2,11 @@ import GlobalProvider from "../components/context/GlobalContext";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-	return;
-	<GlobalProvider>
-		<Component {...pageProps} />
-	</GlobalProvider>;
+	return (
+		<GlobalProvider>
+			<Component {...pageProps} />
+		</GlobalProvider>
+	);
 }
 
 export default MyApp;
