f7d318a648c8775fbe73de58f0ce704c418229c4
Sher Yew <tansheryew@gmail.com>
2022-10-08 19:28:45 +0800

    Updated SideBar.js

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,12 +1,43 @@
-import Indivbox from "./Indivbox.js";
+import Indivbox from "../Indivbox/Indivbox"
+import { useContext, useState } from "react"
+import { GlobalContext } from "../../context/GlobalContext";
 
 const Sidebar = () => {
-	return (
-		//I haven't add the media query
-		<div className="w-1/6 h-full border border-slate-200">
-			<Indivbox walletAdd="1" />
-		</div>
-	);
-};
+
+    const {setWalletAddress} = useContext(GlobalContext)
+    const [data, setdata] = useState({
+        address: "",
+    });
+
+    const accountChangeHandler = (account) => {
+        // Setting an address data
+        var newAddress = account.slice(0,5) + '...' + account.substring(37, account.length);
+        setWalletAddress(newAddress);
+        setdata({
+            address: newAddress
+        });
+    };
+
+    const metamaskConnect = () => {
+        if (window.ethereum) {
+            window.ethereum
+                .request({ method: "eth_requestAccounts" })
+                .then((res) => accountChangeHandler(res[0]));
+        } else {
+            alert("install Metamask extension!")
+        }
+    }
+
+    return (
+        //I haven't add the media query
+        <div className="w-1/6 h-full border border-slate-200 flex flex-col align-center justify-between">
+            <div>
+                <Indivbox walletAdd={`${data.address}`}/>
+                <Indivbox walletAdd="2"/>
+            </div>
+           <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-2 rounded" onClick={metamaskConnect}>Connect</button>
+        </div>
+    )
+}
 
 export default Sidebar;
